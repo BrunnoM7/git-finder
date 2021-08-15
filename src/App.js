06d6f025ba8810f0f6864025833a7ff9e363c361
BrunnoM7 <brunno.marchetti7@gmail.com>
2021-08-15 14:54:33 -0300

    added react-router-dom, created About page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import axios from 'axios'
 
-import Navbar from './components/layout/Navbar';
+import About from './components/pages/About'
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Navbar from './components/layout/Navbar';
 import Alert from './components/layout/Alert'
 
 import './App.css';
@@ -50,20 +52,38 @@ class App extends Component {
     const { users, loading } = this.state;
 
     return (
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Alert alert={this.state.alert} />
-          <Search 
-            searchUsers={this.searchUsers} 
-            clearUsers={this.clearUsers} 
-            showClear={users.length > 0 ? true : false}
-            setAlert={this.setAlert}
-          />
-          <Users loading={loading} users={users} />
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route 
+                exact 
+                path='/' 
+                render={props => (
+                  <Fragment>
+                    <Search 
+                      searchUsers={this.searchUsers} 
+                      clearUsers={this.clearUsers} 
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              />
+              
+              <Route 
+                exact
+                path='/about'
+                component={About}
+              />
+            </Switch>
+            
+          </div>
         </div>
-        
-      </div>
+      </BrowserRouter>
     );
   }
 }
