629bd6e7427f6e8f4f74a726f6557ade5ecb7d11
BrunnoM7 <brunno.marchetti7@gmail.com>
2021-08-18 23:15:26 -0300

    added alert context

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,12 +1,15 @@
 import React, { useState, useContext } from 'react'
-import PropTypes from 'prop-types'
 import GithubContext from '../../context/github/githubContext';
+import AlertContext from '../../context/alert/alertContext';
 
-const Search = ({ setAlert }) => {
+
+const Search = () => {
 
   const githubContext = useContext(GithubContext);
+  const alertContext = useContext(AlertContext);
 
   const { clearUsers, users } = githubContext;
+  const { setAlert } = alertContext;
 
   const [text, setText] = useState(''); 
 
@@ -43,10 +46,6 @@ const Search = ({ setAlert }) => {
     </div>
   )
   
-}
-
-Search.propTypes = {
-  setAlert: PropTypes.func.isRequired
 }
 
 
