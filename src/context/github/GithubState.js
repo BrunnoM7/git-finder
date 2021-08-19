77b5b33cb72c9ae70678cb01c45570a110940e65
BrunnoM7 <brunno.marchetti7@gmail.com>
2021-08-19 15:21:12 -0300

    fixed api requests to use https

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -35,7 +35,7 @@ const GithubState = props => {
   const searchUsers = async text => {
     setLoading();
 
-    const res = await axios.get(`http://api.github.com/search/users?q=${text}&client_id=${githubClientId}&client_secret=${githubClientSecret}`);
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${githubClientId}&client_secret=${githubClientSecret}`);
 
     dispatch({ 
       type: SEARCH_USERS,
@@ -47,7 +47,7 @@ const GithubState = props => {
   const getUser = async username => {
     setLoading();
 
-    const res = await axios.get(`http://api.github.com/users/${username}?client_id=${githubClientId}&client_secret=${githubClientSecret}`);
+    const res = await axios.get(`https://api.github.com/users/${username}?client_id=${githubClientId}&client_secret=${githubClientSecret}`);
 
     dispatch({
       type: GET_USER,
@@ -59,7 +59,7 @@ const GithubState = props => {
   const getUserRepos = async username => {
     setLoading();
 
-    const res = await axios.get(`http://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${githubClientId}&client_secret=${githubClientSecret}`);
+    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${githubClientId}&client_secret=${githubClientSecret}`);
 
     dispatch({
       type: GET_REPOS,
